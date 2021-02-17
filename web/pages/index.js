421c8f95fcff112f977f50c4f45c36a84563ed05
andrew_l <andrewlevins@gmail.com>
2021-02-16 23:11:36 -0500

    clean up homepage markup and css

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import groq from 'groq';
 import client from '../client';
 
-import styles from './homepage.module.css';
+import styles from './homepage.module.scss';
 
 const Index = props => {
   const { posts = [], projects = [] } = props;
@@ -12,15 +12,15 @@ const Index = props => {
   return (
     <main className={styles.main}>
       <Head>
-        <title>My New App main page title</title>
+        <title>Andrew Levinson: Design, Code, Data</title>
         {/* <link rel='icon' href='/favicon.ico' /> */}
       </Head>
       <h1>Andrew Levinson</h1>
-      <h3 className={styles.subhed}>Designer / Developer</h3>
+      <h3 className={styles.subhed}>Designer / Developer / Data Visualizer</h3>
       <p className={styles.intro}>
-        At WSJ, I currently focus on designing and coding reader experiences that usually feature interactive data
-        visualization. Additionally, I sometimes teach communication design at Parsons. Previously, I’ve been a product
-        designer at a software agency, a financial consultant, and even a touring musician in a metal band.
+        Currently, I design & code visuals at The Wall Street Journal and teach Core Lab Interaction at Parsons School
+        of Design. Previously, I’ve been a product designer at a software agency, a financial consultant, and even a
+        touring musician in a metal band.
       </p>
 
       <div className={styles.tree}>
@@ -43,17 +43,19 @@ const Index = props => {
               return (
                 slug && (
                   <li key={_id}>
-                    <span style={{ whiteSpace: 'nowrap', marginBottom: '2rem', display: 'block' }}>
-                      <span>{i === projects.length - 1 ? '└──' : '├──'}</span>
+                    <span className={styles.markers}>
+                      {i === 0 ? '┌──' : i === projects.length - 1 ? '└──' : '├──'}
+                    </span>
+                    <span>
                       <Link href={directLink}>
-                        <a style={{ whiteSpace: 'normal' }}>
+                        <a>
                           {title} <span className={styles.tag}>{`• ${category}`}</span>
                           <span className={styles.description}>{description}</span>
                         </a>
                       </Link>
                       {relatedPost && (
                         <>
-                          <br /> &nbsp; &nbsp; └──
+                          <br /> <span className={styles.markers}>└──</span>
                           <Link href='/post/[slug]' as={`/post/${relatedPost.current}`} passHref>
                             <a style={{ fontSize: 16 }}>Related blog post ⟶</a>
                           </Link>
@@ -75,22 +77,22 @@ const Index = props => {
             ({ _id, title = '', slug = '', publishedAt = '', description = '', directLink = '' }, i) =>
               slug && (
                 <li key={_id}>
-                  <span style={{ whiteSpace: 'nowrap', marginBottom: '2rem', display: 'block' }}>
-                    <span>{i === posts.length - 1 ? '└──' : '├──'}</span>
+                  <span className={styles.markers}>{i === 0 ? '┌──' : i === posts.length - 1 ? '└──' : '├──'}</span>
+                  <span>
                     <Link href='/post/[slug]' as={`/post/${slug.current}`} passHref>
-                      <a style={{ whiteSpace: 'normal' }}>
+                      <a>
                         {title}
                         <span className={styles.description}>{description}</span>
                       </a>
                     </Link>
-                    {directLink && (
+                    {/* {directLink && (
                       <>
                         <br /> &nbsp; &nbsp; └──
                         <Link href={directLink}>
                           <a style={{ fontSize: 16 }}>Direct link ⟶</a>
                         </Link>
                       </>
-                    )}
+                    )} */}
                   </span>
                 </li>
               )
